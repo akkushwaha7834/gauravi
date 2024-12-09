83a277edbb7b2c93fd9b198b39ff974d17f0b794
akkushwaha7834 <akkushwaha018@gmail.com>
2024-12-09 16:22:10 +0530

    Twilio file removed

--- application/config/twilio.php
+++
@@ -1,5 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
-
-$config['twilio_account_sid'] = 'AC760b22146468d458a3f5733d6caecd3d';
-$config['twilio_auth_token'] = '1c8bf883f7375be9d127e6bf7659af3c';
-$config['twilio_phone_number'] = '+918076369563';
